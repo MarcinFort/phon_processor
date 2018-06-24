68dbc37d9ddcd5bb65a7bd52c4c47cda0e2849e7
Marcin Fortuna <marcin.fort@gmail.com>
2018-06-24 18:04:15 +0200

    Add the feature picker component

---
+++ src/FeaturePicker.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+export class FeaturePicker extends React.Component {
+
+    render() {
+        return (
+        <select>
+            <option value="syllabic">syllabic</option>
+            <option value="stress">stress</option>
+            <option value="long">long</option>
+            <option value="consonantal">consonantal</option>
+            <option value="sonorant">sonorant</option>
+            <option value="continuant">continuant</option>
+            <option value="delayed release">delayed release</option>
+            <option value="approximant">approximant</option>
+            <option value="tap">tap</option>
+            <option value="trill">trill</option>
+            <option value="nasal">nasal</option>
+            <option value="voice">voice</option>
+            <option value="spread gl">spread gl</option>
+            <option value="constr gl">constr gl</option>
+            <option value="labial">labial</option>
+            <option value="round">round</option>
+            <option value="labiodental">labiodental</option>
+            <option value="coronal">coronal</option>
+            <option value="anterior">anterior</option>
+            <option value="distributed">distributed</option>
+            <option value="strident">strident</option>
+            <option value="lateral">lateral</option>
+            <option value="dorsal">dorsal</option>
+            <option value="high">high</option>
+            <option value="low">low</option>
+            <option value="front">front</option>
+            <option value="back">back</option>
+            <option value="tense">tense</option>
+        </select>)
+    }
+
+}
