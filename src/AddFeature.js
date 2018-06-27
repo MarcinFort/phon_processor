d8fa5289123d689e6c75b6b5fd0bd227e850e283
Marcin Fortuna <marcin.fort@gmail.com>
2018-06-27 19:30:07 +0200

    Add a button for adding features

---
+++ src/AddFeature.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class AddFeatureButton extends React.Component {
+
+    render() {
+        return <button>Add</button>
+    }
+
+}
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { FeaturePicker } from './FeaturePicker';
 import { ValuePicker } from './ValuePicker';
+import { AddFeatureButton } from './AddFeature';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <ValuePicker />
         <FeaturePicker />
+        <AddFeatureButton />
       </div>
     );
   }
