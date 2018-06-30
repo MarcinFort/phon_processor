7538b133a8b30478e10385f1d8b80d04d3b4544c
Marcin Fortuna <marcin.fort@gmail.com>
2018-07-01 00:38:35 +0200

    Remove redundant state from components

--- src/FeaturePicker.js
+++ src/FeaturePicker.js
@@ -3,50 +3,43 @@ import React from 'react';
 
 export class FeaturePicker extends React.Component {
 
-    state = {
-        value: "syllabic"
-    }
-
     handleChange(event) {
-        this.setState({
-            value: event.target.value
-        })
         this.props.updateCurrentFeature(event.target.value);
-
     }
 
     render() {
         return (
-        <select value={this.state.value} onChange={this.handleChange.bind(this)}>
-            <option value="syllabic">syllabic</option>
-            <option value="stress">stress</option>
-            <option value="long">long</option>
-            <option value="consonantal">consonantal</option>
-            <option value="sonorant">sonorant</option>
-            <option value="continuant">continuant</option>
-            <option value="delayed release">delayed release</option>
-            <option value="approximant">approximant</option>
-            <option value="tap">tap</option>
-            <option value="trill">trill</option>
-            <option value="nasal">nasal</option>
-            <option value="voice">voice</option>
-            <option value="spread gl">spread gl</option>
-            <option value="constr gl">constr gl</option>
-            <option value="labial">labial</option>
-            <option value="round">round</option>
-            <option value="labiodental">labiodental</option>
-            <option value="coronal">coronal</option>
-            <option value="anterior">anterior</option>
-            <option value="distributed">distributed</option>
-            <option value="strident">strident</option>
-            <option value="lateral">lateral</option>
-            <option value="dorsal">dorsal</option>
-            <option value="high">high</option>
-            <option value="low">low</option>
-            <option value="front">front</option>
-            <option value="back">back</option>
-            <option value="tense">tense</option>
-        </select>)
+            <select onChange={this.handleChange.bind(this)}>
+                <option value="syllabic">syllabic</option>
+                <option value="stress">stress</option>
+                <option value="long">long</option>
+                <option value="consonantal">consonantal</option>
+                <option value="sonorant">sonorant</option>
+                <option value="continuant">continuant</option>
+                <option value="delayed release">delayed release</option>
+                <option value="approximant">approximant</option>
+                <option value="tap">tap</option>
+                <option value="trill">trill</option>
+                <option value="nasal">nasal</option>
+                <option value="voice">voice</option>
+                <option value="spread gl">spread gl</option>
+                <option value="constr gl">constr gl</option>
+                <option value="labial">labial</option>
+                <option value="round">round</option>
+                <option value="labiodental">labiodental</option>
+                <option value="coronal">coronal</option>
+                <option value="anterior">anterior</option>
+                <option value="distributed">distributed</option>
+                <option value="strident">strident</option>
+                <option value="lateral">lateral</option>
+                <option value="dorsal">dorsal</option>
+                <option value="high">high</option>
+                <option value="low">low</option>
+                <option value="front">front</option>
+                <option value="back">back</option>
+                <option value="tense">tense</option>
+            </select>
+        )
     }
 
 }
--- src/ValuePicker.js
+++ src/ValuePicker.js
@@ -3,27 +3,21 @@ import React from 'react';
 
 export class ValuePicker extends React.Component {
 
-    state = {
-        value: "+"
-    }
-
     handleChange(event) {
-        this.setState({
-            value: event.target.value
-        })
         this.props.updateCurrentValue(event.target.value);
     }
 
     render() {
         return (
-        <select value={this.state.value} onChange={this.handleChange.bind(this)}>
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="alpha">α</option>
-            <option value="-alpha">-α</option>
-            <option value="beta">β</option>
-            <option value="-beta">-β</option>
-        </select>)
+            <select onChange={this.handleChange.bind(this)}>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="alpha">α</option>
+                <option value="-alpha">-α</option>
+                <option value="beta">β</option>
+                <option value="-beta">-β</option>
+            </select>
+        )
     }
 
 }
