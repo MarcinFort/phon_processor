15b45ec31409a0d8a7419f7603d8d7c00349621b
Marcin Fortuna <marcin.fort@gmail.com>
2018-07-01 16:06:19 +0200

    Add a string replace method

--- src/SegmentWindow.js
+++ src/SegmentWindow.js
@@ -3,11 +3,12 @@ import React from 'react';
 export class SegmentWindow extends React.Component {
     
     render() {
+
         return (
             <div>
                 {
                     Object.keys(this.props.features).map(
-                        key => <p>{this.props.features[key]}{key}</p>
+                        key => <p>{this.props.features[key].replace(/alpha/, "α").replace(/beta/, "β")} {key}</p>
                     )
                 }
             </div>
