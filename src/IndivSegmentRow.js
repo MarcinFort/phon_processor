95f93a931d207a24b45d30e7b072b3ab44aa0e2b
Marcin Fortuna <marcin.fort@gmail.com>
2018-07-07 13:35:40 +0200

    Display changed segment IPA in rows

--- src/IndivSegmentRow.js
+++ src/IndivSegmentRow.js
@@ -6,12 +6,15 @@ export class IndivSegmentRow extends React.Component {
     render() {
 
         let values = Object.values(this.props.spec).slice(1);
+        if (values.length === 29) {
+            values.pop();
+        }
 
         return(
             <tr>
                 <td>{this.props.spec.ipa}</td>
                 <td>→</td>
-                <td>?</td>
+                <td>{this.props.spec.ipa2}</td>
                 {values.map((value, index) => <td>{values[index]}</td>)}
             </tr>
         )
