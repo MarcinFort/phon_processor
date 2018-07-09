e26d2bbf132701e1777616a420fd0fd3c6e4d709
Marcin Fortuna <marcin.fort@gmail.com>
2018-07-09 18:58:01 +0200

    Add the SegmentPicker component

--- src/Difference.js
+++ src/Difference.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { SegmentPicker } from './SegmentPicker';
 
 export class Difference extends React.Component {
+
+    state = {
+        sound1: {},
+        sound2: {}
+    }
+
     render() {
         return (
-            <p>See how any two sounds differ in terms of distinctive features</p>
+            <div>
+                <p>See how any two sounds differ in terms of distinctive features</p>
+                <SegmentPicker />
+                <SegmentPicker />
+            </div>
         )
     }
 }
---
+++ src/SegmentPicker.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { featuresAPI } from './data/FeaturesAPI';
+
+export class SegmentPicker extends React.Component {
+
+    handleChange(event) {
+        this.props.pickSegment(event.target.value);
+    }
+
+    render() {
+
+        let ipa_symbols = [];
+        featuresAPI.forEach(x => {
+            ipa_symbols.push(x.ipa);
+        })
+
+        return(
+            <select onChange={this.handleChange.bind(this)}>
+                {ipa_symbols.map(y => <option value={y}>{y}</option>)}
+            </select>
+        )    
+    }
+}
