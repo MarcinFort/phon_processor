e01e0d23c2e9d01efcc6e64cd8f249f62996e218
Marcin Fortuna <marcin.fort@gmail.com>
2018-07-09 20:25:41 +0200

    Add row specification for segment comparison

--- src/ComparisonWindow.js
+++ src/ComparisonWindow.js
@@ -10,11 +10,34 @@ export class ComparisonWindow extends React.Component {
         let fullSpec1 = JSON.parse(JSON.stringify(featuresAPI.filter(x => x.ipa === this.props.sound1)));
         let fullSpec2 = JSON.parse(JSON.stringify(featuresAPI.filter(x => x.ipa === this.props.sound2)));
 
+        if (fullSpec1.length > 0) { delete fullSpec1[0].ipa };
+        if (fullSpec2.length > 0) { delete fullSpec2[0].ipa };
+
+        let rowsSpec = [];
+        if (fullSpec1[0] && fullSpec2[0]) {
+            for (let prop in fullSpec1[0]) {
+                let arr = [];
+                if (fullSpec1[0][prop] === fullSpec2[0][prop]) {
+                    arr.push([fullSpec1[0][prop]+prop]);
+                } else {
+                    arr.push([fullSpec1[0][prop]+prop, fullSpec2[0][prop]+prop]);
+                }
+                rowsSpec.push(arr);
+            }
+        }
+
         return(
-            <div>
-                {JSON.stringify(fullSpec1)}
-                {JSON.stringify(fullSpec2)}
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>{this.props.sound1}</th>
+                        <th>{this.props.sound2}</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
         )
     }
 }
