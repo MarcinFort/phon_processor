a19c25e6a196f89e92955cb23d6fc466a997460a
Marcin Fortuna <marcin.fort@gmail.com>
2018-07-09 19:48:16 +0200

    Add full specifications of segments to the Comparison component

--- src/ComparisonWindow.js
+++ src/ComparisonWindow.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { featuresAPI } from './data/FeaturesAPI';
 
 export class ComparisonWindow extends React.Component {
 
     render() {
 
+        // Deep copies to avoid overwriting the API
+
+        let fullSpec1 = JSON.parse(JSON.stringify(featuresAPI.filter(x => x.ipa === this.props.sound1)));
+        let fullSpec2 = JSON.parse(JSON.stringify(featuresAPI.filter(x => x.ipa === this.props.sound2)));
+
         return(
             <div>
-                {this.props.sound1}
-                {this.props.sound2}
+                {JSON.stringify(fullSpec1)}
+                {JSON.stringify(fullSpec2)}
             </div>
         )
     }
