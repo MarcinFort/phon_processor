99ff0ec00b6570996fc41e123a21aaca23e23578
Marcin Fortuna <marcin.fort@gmail.com>
2018-07-13 16:53:14 +0200

    Add basic consonants to the PickInventory components; render tds

--- src/PickInventory.js
+++ src/PickInventory.js
@@ -6,7 +6,7 @@ export class PickInventory extends React.Component {
 
     render() {
 
-        let basic_consonants = [];
+        let basic_consonants = [["plosive","p","","","t","","ʈ","","c","k","q","","ʔ"],[" ","b","","","d","","ɖ","","ɟ","ɡ","ɢ","",""],["affricate","p͡ɸ","p͡f","t̪͡θ","t͡s","t͡ʃ","ʈ͡ʂ","t͡ɕ","c͡ç","k͡x","q͡χ","",""],[" ","b͡β","b͡v","d̪͡ð","d͡z","d͡ʒ","ɖ͡ʐ","d͡ʑ","ɟ͡ʝ","ɡ͡ɣ","ɢ͡ʁ","",""],["lateral affricate","","","t̪͡ɬ̪","t͡ɬ","t͡ɬ̲","","","","","","",""],[" ","","","d̪͡ɮ̪","d͡ɮ","d̠͡ɮ̠","","","","","","",""],["fricative","ɸ","f","θ","s","ʃ","ʂ","ɕ","ç","x","χ","ħ","h"],[" ","β","v","ð","z","ʒ","ʐ","ʑ","ʝ","ɣ","ʁ","ʕ","ɦ"],["lateral fricative","","","","ɬ","","","","","","","",""],[" ","","","","ɮ","","","","","","","",""],["nasal","m","ɱ","","n","","ɳ","","ɲ","ŋ","ɴ","",""],["trill","ʙ","","","r","","ʀ","","","","ʀ","",""],["flap/tap","","","","ɾ","","ɽ","","","","","",""],["lateral flap","","","","ɺ","","","","","","","",""],["approximant","","ʋ","","ɹ","","ɻ","","j","ɰ","","",""],["lateral approximant","","","","l","","ɭ","","ʎ","ʟ","","",""],[""]];
 
         return(
             <div>
@@ -36,7 +36,7 @@ export class PickInventory extends React.Component {
                 </table>
                 <table id="vowels">
                     <thead>
-
+                        {basic_consonants.map(x => <tr>{x.map(y => <td>{y}</td>)}</tr>)}
                     </thead>
                     <tbody></tbody>
                 </table>
